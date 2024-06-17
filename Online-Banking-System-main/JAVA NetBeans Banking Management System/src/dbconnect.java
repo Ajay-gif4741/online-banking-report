3f2577d08206cfdf41b6d232cd11567a03e2be60
Ajay-gif4741 <modiboinaajay6@gmail.com>
2024-06-17 11:42:20 +0530

    Add files via upload

---
+++ Online-Banking-System-main/JAVA NetBeans Banking Management System/src/dbconnect.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class dbconnect {
+    Connection conn = null;
+/* In this section please change the path to connect to the database.
+*/
+    public static Connection connectDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:bank.sqlite");
+            
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+        }
+        return null;
+    }
+    
+}
